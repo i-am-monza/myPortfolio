17f386f6e00d7b7826b4ddf2c15e48fc9b8475e8
Monza <monwabisidingane@gmail.com>
2019-11-11 12:03:56 +0200

    Updates

---
+++ precache-manifest.2d6e0cd91f6ebccdfee77cda564eb01b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbfef2eb3694120ee8bed0a0eae2e00",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "04b3a5ef432d4744abd5",
+    "url": "/myPortfolio/static/css/main.c163e64e.chunk.css"
+  },
+  {
+    "revision": "e56a795939e41ea99cb0",
+    "url": "/myPortfolio/static/js/2.d7c07f0e.chunk.js"
+  },
+  {
+    "revision": "04b3a5ef432d4744abd5",
+    "url": "/myPortfolio/static/js/main.4b59b182.chunk.js"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "90c5a6389497f089fa3bc67b15322249",
+    "url": "/myPortfolio/static/media/black-wood.90c5a638.png"
+  },
+  {
+    "revision": "81a4e1b61c2985a37e5baa3a3115292e",
+    "url": "/myPortfolio/static/media/iammonza.81a4e1b6.png"
+  },
+  {
+    "revision": "ab9a5013e6f137146b184c229a94a8d5",
+    "url": "/myPortfolio/static/media/light-wood.ab9a5013.png"
+  }
+]);
